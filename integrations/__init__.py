eca48cecdcb0545525388930dc994293a05284a7
diywang <wang.di@northeastern.edu>
2026-01-22 14:54:24 +0800

    refactor: move feishu_doc.py to integrations/ directory
    - Move feishu_doc.py to integrations/feishu_doc.py
    - Update import in main.py
    - Add integrations/__init__.py
    - Update CI workflow to include integrations/ directory

---
+++ integrations/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Integration modules
+
+This package contains third-party service integrations:
+- feishu_doc: Feishu document management
+"""
+
+from integrations.feishu_doc import FeishuDocManager
+
+__all__ = [
+    'FeishuDocManager',
+]
